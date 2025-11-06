0395204fd7b0ea198c248ce10aed29a21202b117
OptiWealthSolutions <optiwealth.solutions@gmail.com>
2025-11-06 02:08:44 -0800

    maj

--- src/strategy.py
+++ src/strategy.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import pandas_ta as ta # Importe la bibliothèque pandas_ta
 import numpy as np
 
 def add_moving_average_signals(df, short_window=20, long_window=50):
@@ -7,17 +6,17 @@ def add_moving_average_signals(df, short_window=20, long_window=50):
     if 'close' not in df.columns:
         raise ValueError("Le DataFrame doit contenir une colonne 'close'.")
 
-
     df_strat = df.copy()
 
 
-    df_strat['SMA_short'] = df_strat.ta.sma(length=short_window)
-    df_strat['SMA_long'] = df_strat.ta.sma(length=long_window)
+    df_strat['SMA_short'] = df_strat['close'].rolling(window=short_window).mean()
+    df_strat['SMA_long'] = df_strat['close'].rolling(window=long_window).mean()
     
-    print(f"Moyennes mobiles (Courte={short_window}, Longue={long_window}) calculées.")
+    print(f"Moyennes mobiles (Courte={short_window}, Longue={long_window}) calculées avec pandas.")
+
 
     df_strat['signal'] = 0
-    
+   
 
     condition_achat = (df_strat['SMA_short'] > df_strat['SMA_long']) & \
                       (df_strat['SMA_short'].shift(1) <= df_strat['SMA_long'].shift(1))
